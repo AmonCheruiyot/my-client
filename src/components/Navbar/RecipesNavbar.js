56331fa9ac8f3c39217c71fcae16891358d4f05d
Amon <kiprotichamon03@gmail.com>
2024-08-11 21:55:59 +0300

    updates

--- src/components/Navbar/RecipesNavbar.js
+++ src/components/Navbar/RecipesNavbar.js
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom';
 const RecipesNavbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/wishlist">Wishlist</Link>
+      <div className="logo">Recipe Haven</div> {/* Optional: Add a logo */}
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/wishlist">Wishlist</Link>
+      </div>
     </nav>
   );
 }
